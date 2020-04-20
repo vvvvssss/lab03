f74bb28d1b5f41bcfa98593204a6c7591e21a4e8
Вадим Шепелин <v.schepelin@yandex.ru>
2020-04-20 12:54:41 +0300

    code:Значения параметров

--- main.cpp
+++ main.cpp
@@ -49,6 +49,8 @@ vector <size_t> make_histogram(const vector<double> &numbers,size_t bin_count)
     }
     return bins;
 }
+
+
 void show_histogram_text(const vector<size_t> &bins)
 {
 
@@ -92,6 +94,18 @@ void show_histogram_text(const vector<size_t> &bins)
     }
 }
 
+
+
+
+void svg_rect(double x, double y, double width, double height,string stroke = "black",string fill = "black")
+{
+    cout << "<rect x='"<<x<<"' y='" <<y<<"' width='" <<width <<"' height='" <<height <<"' stroke='"<<stroke<<"' fill='"<<fill<<"'/>";
+}
+
+void svg_text(double left, double baseline, string text)
+{
+    cout << "<text x='" << left << "' y='" << baseline << "'>" << text << "</text>";
+}
 void
 svg_begin(double width, double height)
 {
@@ -102,29 +116,32 @@ svg_begin(double width, double height)
     cout << "viewBox='0 0 " << width << " " << height << "' ";
     cout << "xmlns='http://www.w3.org/2000/svg'>\n";
 }
-
 void
 svg_end()
 {
     cout << "</svg>\n";
 }
-
-
-void svg_text(double left, double baseline, string text)
-{
-    cout << "<text x='" << left << "' y='" << baseline << "'>" << text << "</text>";
-}
-
 void
 show_histogram_svg(const vector<size_t>& bins)
 {
-    svg_begin(400, 300);
-    svg_text(20, 20, to_string(bins[0]));
-    svg_end();
+    const auto IMAGE_WIDTH = 400;
+    const auto IMAGE_HEIGHT = 300;
+    const auto TEXT_LEFT = 20;
+    const auto TEXT_BASELINE = 20;
+    const auto TEXT_WIDTH = 50;
+    const auto BIN_HEIGHT = 30;
+    const auto BLOCK_WIDTH = 10;
+    svg_begin(IMAGE_WIDTH,IMAGE_HEIGHT);
+    double top = 0;
+    for (size_t bin : bins)
+    {
+        const double bin_width = BLOCK_WIDTH * bin;
+        svg_text(TEXT_LEFT, top + TEXT_BASELINE, to_string(bin));
+        svg_rect(TEXT_WIDTH, top, bin_width, BIN_HEIGHT,"darkviolet","#9400D3");
+        top += BIN_HEIGHT;
+    }
+
 }
-
-
-
 int main()
 {
     size_t number_count;
